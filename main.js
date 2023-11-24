c387a85bdc260b78ee3e5ffdbbabb035a7ac634f
Bhikesh Khute <35907619+norfluxX@users.noreply.github.com>
2023-11-24 07:40:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
+//const accountSid = process.env.TWILIO_ACCOUNT_SID;
+//const authToken = process.env.TWILIO_AUTH_TOKEN;
+//const client = require('twilio')(accountSid, authToken);
 
-client.messages
-      .create({
-         from: 'whatsapp:+14155238886',
-         body: 'Hello, there!',
-         to: 'whatsapp:+15005550006'
-       })
-      .then(message => console.log(message.sid));
+//client.messages
+//      .create({
+//         from: 'whatsapp:+14155238886',
+//         body: 'Hello, there!',
+//         to: 'whatsapp:+15005550006'
+//       })
+//      .then(message => console.log(message.sid));
 // Note - The above code is for Whatsapp and below code is for SMS.
 // FOR SMS
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
